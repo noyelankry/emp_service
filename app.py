2b0a6cea62551ca12ee07186e76020067fa332fa
noyelankry <noy.elankry@gmail.com>
2023-08-02 17:36:49 +0300

    update config to env vars

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from api.models.employee import db
 from api.routes.employee_routes import employee_bp
+from decouple import config
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
-app.config['SECRET_KEY'] = 'random string'
+app.config['SQLALCHEMY_DATABASE_URI'] = config('DATABASE_URI')
+app.config['SECRET_KEY'] = config('SECRET_KEY')
 
 db.init_app(app)
 
